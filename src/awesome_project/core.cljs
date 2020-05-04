a895c377a1ef567995dd3a52dbc42140525c0b9c
Oliver George <oliver@condense.com.au>
2020-05-04 15:52:42 +1000

    repro

--- src/awesome_project/core.cljs
+++ src/awesome_project/core.cljs
@@ -2,7 +2,9 @@
   (:require [reagent.core :as r]
             [reagent.react-native :as rn]))
 
+(goog-define greeting "Hello")
+
 (defn ^:export -main [& args]
   (r/as-element
     [rn/view {:style {:flex 1 :align-items "center" :justify-content "center"}}
-     [rn/text {:style {:font-size 50}} "Hello Krell!"]]))
+     [rn/text {:style {:font-size 50}} greeting " Krell!"]]))
