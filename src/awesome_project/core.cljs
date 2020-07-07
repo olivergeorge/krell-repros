9ca4385d1b5102464693df0c6d184ea0a4430ca2
Oliver George <oliver@condense.com.au>
2020-07-07 14:00:52 +1000

    POC

--- src/awesome_project/core.cljs
+++ src/awesome_project/core.cljs
@@ -1,24 +1,32 @@
 (ns awesome-project.core
   (:require [reagent.core :as r]
             [reagent.react-native :as rn]
-            [goog.object :as gobject]))
+            [goog.object :as gobject]
+            [react-native-camera :as module]))
+
+(def RNCamera module/RNCamera)
 
 (defn app []
-  [rn/view {:style {:flex 1 :align-items "center" :justify-content "center"}}
-   [rn/text {:style {:font-size 50}} "Hello Krell!"]])
+      [rn/view
+       {:style {:flex 1 :flexDirection "column" :background-color "black"}}
+       [:> RNCamera
+        {:style         {:flex 1 :flexDirection "column" :background-color "black"}
+         :type          module/RNCamera.Constants.FlashMode.on
+         :captureAudio  false
+         :onBarCodeRead (fn [data] (js/console.log (pr-str ::onBarCodeRead) data))}]])
 
 (defn error-report [error]
-  [rn/view {:style {:padding-horizontal 20 :background-color "pink" :flex 1}}
-   [rn/text {:style {:padding-top 80 :font-size 32 :color "red"}} "Error report"]
-   [rn/text {:style {:padding-top 20 :font-size 14 :color "black"}} (str error)]
-   [rn/text {:style {:padding-top 10 :font-size 12 :color "black"}} (str (gobject/get error "componentStack"))]])
+      [rn/view {:style {:padding-horizontal 20 :background-color "pink" :flex 1}}
+       [rn/text {:style {:padding-top 80 :font-size 32 :color "red"}} "Error report"]
+       [rn/text {:style {:padding-top 20 :font-size 14 :color "black"}} (str error)]
+       [rn/text {:style {:padding-top 10 :font-size 12 :color "black"}} (str (gobject/get error "componentStack"))]])
 
 (defn error-boundary []
-  (let [*error (r/atom nil)]
-    (r/create-class
-      {:get-derived-state-from-error (fn [e] (reset! *error e))
-       :component-did-catch          (fn [_ _ _] true)
-       :reagent-render               (fn [form] (if @*error [error-report @*error] form))})))
+      (let [*error (r/atom nil)]
+           (r/create-class
+             {:get-derived-state-from-error (fn [e] (reset! *error e))
+              :component-did-catch          (fn [_ _ _] true)
+              :reagent-render               (fn [form] (if @*error [error-report @*error] form))})))
 
 (defn ^:export -main [& args]
-  (r/as-element [error-boundary [app]]))
+      (r/as-element [error-boundary [app]]))
