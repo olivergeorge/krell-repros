69eed4448a804891938e8e0520fed33d85df264e
Oliver George <oliver@condense.com.au>
2020-07-19 14:18:16 +1000

    Fresh setup

--- src/awesome_project/core.cljs
+++ src/awesome_project/core.cljs
@@ -1,17 +1,18 @@
 (ns awesome-project.core
   (:require [reagent.core :as r]
             [reagent.react-native :as rn]
-            [goog.object :as gobject]))
+            [goog.object :as gobject]
+            [goog.events.BrowserEvent]))
 
 (defn app []
   [rn/view {:style {:flex 1 :align-items "center" :justify-content "center"}}
    [rn/text {:style {:font-size 50}} "Hello Krell!"]])
 
 (defn error-report [error]
-  [rn/view {:style {:padding-horizontal 20 :background-color "pink" :flex 1}}
-   [rn/text {:style {:padding-top 80 :font-size 32 :color "red"}} "Error report"]
-   [rn/text {:style {:padding-top 20 :font-size 14 :color "black"}} (str error)]
-   [rn/text {:style {:padding-top 10 :font-size 12 :color "black"}} (str (gobject/get error "componentStack"))]])
+      [rn/view {:style {:padding-horizontal 20 :background-color "pink" :flex 1}}
+       [rn/text {:style {:padding-top 80 :font-size 32 :color "red"}} "Error report"]
+       [rn/text {:style {:padding-top 20 :font-size 14 :color "black"}} (str error)]
+       [rn/text {:style {:padding-top 10 :font-size 12 :color "black"}} (str (gobject/get error "componentStack"))]])
 
 (defn error-boundary []
   (let [*error (r/atom nil)]
@@ -21,4 +22,4 @@
        :reagent-render               (fn [form] (if @*error [error-report @*error] form))})))
 
 (defn ^:export -main [& args]
-  (r/as-element [error-boundary [app]]))
+  (r/as-element [hello]))
